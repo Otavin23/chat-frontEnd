c5b37cdfb4c9abe33fa3c4bac3950dae137a70c1
Otavin23 <alvesotavio132@gmail.com>
2023-03-27 21:29:38 -0300

    listChatMessage finished style

--- src/pages/message/index.tsx
+++ src/pages/message/index.tsx
@@ -4,6 +4,7 @@ import MessageImage from "../../assets/chat/sms.png";
 import SquareMenuImage from "../../assets/chat/squares.png";
 import ConfigImage from "../../assets/chat/config.png";
 import UserImage from "../../assets/chat/usuario.png";
+import bloomImage from "../../assets/listChatMessages/pesquisa.png";
 
 const Message = () => {
   return (
@@ -29,8 +30,56 @@ const Message = () => {
             <img src={UserImage} alt="" width="40" height="40" />
           </S.userAccount>
         </S.menuAside>
-        <S.listChatMessage>2</S.listChatMessage>
-        <S.chatUserPerson>3</S.chatUserPerson>
+
+        <S.listChatMessages>
+          <S.inputSearchPersonMessages>
+            <img src={bloomImage} alt="" width="25" height="25" />
+            <input type="text" placeholder="Search" />
+          </S.inputSearchPersonMessages>
+
+          <S.listPeopleMessages>
+            <li className="li__activeLocalMessage">
+              <img src={UserImage} alt="" width="50" height="50" />
+              <div className="information__nameAbout">
+                <h4 className="title__namePersonMessage">Calos</h4>
+                <p className="title_descriptionAboutProject">
+                  Desenvolvedor Java
+                </p>
+              </div>
+            </li>
+
+            <li>
+              <img src={UserImage} alt="" width="50" height="50" />
+              <div className="information__nameAbout">
+                <h4 className="title__namePersonMessage">Gabriel</h4>
+                <p className="title_descriptionAboutProject">
+                  Desenvolvedor Java
+                </p>
+              </div>
+            </li>
+
+            <li>
+              <img src={UserImage} alt="" width="50" height="50" />
+              <div className="information__nameAbout">
+                <h4 className="title__namePersonMessage">Felipe</h4>
+                <p className="title_descriptionAboutProject">
+                  Desenvolvedor Java
+                </p>
+              </div>
+            </li>
+          </S.listPeopleMessages>
+        </S.listChatMessages>
+
+        <S.chatUserPerson>
+          <S.chatUserInformationPerson>
+            <span>
+              <img src={UserImage} alt="" />
+            </span>
+            <h4>Carlos</h4>
+          </S.chatUserInformationPerson>
+
+          <S.mainChatMessage></S.mainChatMessage>
+        </S.chatUserPerson>
       </S.chatContent>
     </S.mainBackground>
   );
--- src/pages/message/style.ts
+++ src/pages/message/style.ts
@@ -15,7 +15,7 @@ export const chatContent = styled.div`
 export const menuAside = styled.aside`
   background: #081C34;
   height: 100%;
-  width: 4.5%;
+  width: 90px;
   border-radius: 1rem;
   display: flex;
   justify-content: space-between;
@@ -54,7 +54,82 @@ export const listOptions = styled.ul`
 
 export const userAccount = styled.div``
 
-export const listChatMessage = styled.section``
+export const listChatMessages = styled.section`
+  padding: 1rem;
+  width: 350px;
+`
+
+export const inputSearchPersonMessages = styled.div`
+  border: 2px solid #E7E7E7;
+  width: 100%;
+  height: 50px;
+  border-radius: 0.5rem;
+  display: flex;
+  align-items: center;
+  img{
+    margin: 0 0.5rem;
+  }
+
+  input{
+    height: 100%;
+    width: 100%;
+    border: none;
+    border-radius: 0.5rem;
+    outline: none;
+    color: black;
+    font-weight: 500;
+    font-size: 16px;
+    padding: 0 0.5rem 0 0;
+  }
+  
+  input::placeholder{
+    color: #CBCBCB;
+    font-weight: 400;
+  }
+`
+
+export const listPeopleMessages = styled.ul`
+  list-style: none;
+  li{
+    background: #E9E9F3;
+    margin: 1.5rem 0;
+    display: flex;
+    align-items: center;
+    padding: 1.5rem 1.5rem;
+    border-radius: 0.5rem;
+    cursor: pointer;
+
+    .information__nameAbout{
+      margin: 0 0 0 0.5rem;
+
+      .title__namePersonMessage{
+        font-size: 18px;
+        color: black;
+        font-weight: 500;
+      }
+
+      .title_descriptionAboutProject{
+        margin: 0.3rem 0 0 0;
+        color: #CBCBCB;
+      }
+    }
+
+  }
+
+  .li__activeLocalMessage{
+    background: #6785FF;
+
+    .information__nameAbout{
+      .title__namePersonMessage{
+        color: #fff;
+      }
+    }
+  }
+
+`
 
 export const chatUserPerson = styled.div``
 
+export const  chatUserInformationPerson = styled.div``
+
+export const mainChatMessage = styled.div``
